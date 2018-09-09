65e88c5df3a89c94fe6de268e658f90463894a1f
SimonGodefroid <simon.godefroid@gmail.com>
2018-09-09 17:58:49 +0200

    [api_auth] Add handler for creating user

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const chalk = require('chalk');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/API_AUTHENTICATION",{ useNewUrlParser: true });
+
 const app = express();
 
 // Middlewares
@@ -10,6 +16,7 @@ app.use(bodyParser.json());
 // Routes
 app.use('/users',require('./routes/userRoutes'));
 
+console.log();
 // http://localhost:3000/users/signup
 // http://localhost:3000/users/signin
 // http://localhost:3000/users/secret
@@ -17,4 +24,4 @@ app.use('/users',require('./routes/userRoutes'));
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port);
-console.log(`Server listening at ${port}`)
+console.log(chalk.blue(`Server listening at ${port}`));
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,21 @@
+const User = require('../models/User');
+
 module.exports = {
 	signUp: async (req, res, next) => {
-    // email and password
-		console.log('Contents of req.value.body',req.value.body);
 		console.log('UsersController.signUp() called');
+		const { email, password } = req.value.body;
+		// Check if there is a user with the same email
+		const foundUser = await User.findOne({ email });
+		if (foundUser) {
+			return res.status(403).send({ error: 'Email is already in use' });
+		}
+		// Create a new user
+		const newUser = new User({ email, password });
+		await newUser.save();
+
+		// Respond with token
+		res.json({ user: 'created', success: true });
 	},
-	signIn: async (req, res, next) => {
-    // generate token
-		console.log('UsersController.signIn() called');
-	},
-	secret: async (req, res, next) => {
-		console.log('UsersController.secret() called');
-	},
+	signIn: async (req, res, next) => {},
+	secret: async (req, res, next) => {},
 };
